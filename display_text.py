75a48ac642c3431edc3206126ba2c1b7a8af456a
ka10ryu1 <user.email=lsyp.tuiovjo.1231@gmail.com>
2024-09-03 17:31:47 +0900

    🎉 initial commit

---
+++ display_text.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import st7735
+from fonts.ttf import RobotoMedium as UserFont
+from PIL import Image, ImageDraw, ImageFont
+
+
+class DisplayText:
+    def __init__(self):
+        # Create LCD class instance.
+        self.disp = st7735.ST7735(
+            port=0, cs=1, dc="GPIO9", backlight="GPIO12", rotation=270, spi_speed_hz=10_000_000
+        )
+        # Initialize display.
+        self.disp.begin()
+
+        self.img = None
+        self.draw = None
+
+    def draw_text(self, text, canvas_color=(0, 0, 0)):
+        # Width and height to calculate text position.
+        img_size = (self.disp.width, self.disp.height)
+
+        # New canvas to draw on.
+        self.img = Image.new("RGB", img_size, color=canvas_color)
+        self.draw = ImageDraw.Draw(self.img)
+
+        # Text settings.
+        font_size = 25
+        font = ImageFont.truetype(UserFont, font_size)
+        text_color = (255, 255, 255)
+        back_color = (0, 170, 170)
+
+        x1, y1, x2, y2 = font.getbbox(text)
+        size_x = x2 - x1
+        size_y = y2 - y1
+
+        # Calculate text position
+        w, h = self.img.size
+        x = (w - size_x) / 2
+        y = (h / 2) - (size_y / 2)
+
+        # Draw background rectangle and write text.
+        self.draw.rectangle((0, 0, 160, 80), back_color)
+        self.draw.text((x, y), text, font=font, fill=text_color)
+        return 0
+
+    def display_clear(self):
+        self.disp.set_backlight(0)
+        return 0
---
+++ weather_viewer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+
+import logging
+import time
+import argparse
+from pathlib import Path
+
+from bme280 import BME280
+from smbus2 import SMBus
+
+from weather_custom import command, device_setup, get_value
+from display_text import DisplayText
+
+
+def main(args):
+
+    bme280 = device_setup()
+    display = DisplayText()
+    # 最初の値は捨てる
+    get_value(bme280, 2, 1)
+    logging.info("Setup finished. Start reading values...")
+    display.draw_text("Hello World!")
+
+    try:
+        while True:
+            temperature, pressure, humidity = get_value(bme280, args.average_num, args.interval)
+            logging.info(
+                f"Temperature: {temperature:5.2f} °C / Pressure: {pressure:7.2f} hPa / Relative humidity: {humidity:5.2f} %"
+            )
+
+    except KeyboardInterrupt:
+        logging.info("Exit.")
+        display.display_clear()
+
+
+if __name__ == '__main__':
+    logging.basicConfig(
+        format="%(asctime)s.%(msecs)03d %(levelname)-8s %(message)s",
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+
+    logging.info(
+        "weather.py - Print readings from the BME280 weather sensor. Press Ctrl+C to exit!"
+    )
+    main(command())
