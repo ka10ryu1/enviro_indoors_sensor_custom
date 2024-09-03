37482342189ae768a6368848b894cab70e458787
ka10ryu1 <user.email=lsyp.tuiovjo.1231@gmail.com>
2024-09-03 17:55:46 +0900

    ✨ テキストのX座標を固定し、Y座標は可変にした

--- display_text.py
+++ display_text.py
@@ -12,11 +12,12 @@ class DisplayText:
         )
         # Initialize display.
         self.disp.begin()
+        self.draw_text("Initializing...", y_pos=30)
 
         self.img = None
         self.draw = None
 
-    def draw_text(self, text, font_size=25, canvas_color=(0, 0, 0)):
+    def draw_text(self, text, font_size=25, y_pos=10, canvas_color=(0, 0, 0)):
         # Width and height to calculate text position.
         img_size = (self.disp.width, self.disp.height)
 
@@ -29,18 +30,9 @@ class DisplayText:
         text_color = (255, 255, 255)
         back_color = (0, 170, 170)
 
-        x1, y1, x2, y2 = font.getbbox(text)
-        size_x = x2 - x1
-        size_y = y2 - y1
-
-        # Calculate text position
-        w, h = self.img.size
-        x = (w - size_x) / 2
-        y = (h / 2) - (size_y / 2)
-
         # Draw background rectangle and write text.
         self.draw.rectangle((0, 0, 160, 80), back_color)
-        self.draw.text((x, y), text, font=font, fill=text_color)
+        self.draw.text((10, y_pos), text, font=font, fill=text_color)
         self.disp.display(self.img)
         return 0
 
